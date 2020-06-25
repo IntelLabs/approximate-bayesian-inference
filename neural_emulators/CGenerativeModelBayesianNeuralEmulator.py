c891161f5628832ab2660502c05ad04ed5c9f3cf
Javier Felip Leon <javier.felip.leon@intel.com>
2020-06-24 17:18:53 -0700

    update vi deps
    Former-commit-id: b220cd95b88c8dc9b879634d84ec52f6c7c3a9b0

---
+++ neural_emulators/CGenerativeModelBayesianNeuralEmulator.py
@@ -0,0 +1,54 @@
+from common.common import *
+from neural_emulators.CBaseGenerativeNeuralEmulator import CBaseGenerativeNeuralEmulator
+
+
+class CGenerativeModelNeuralEmulator(CBaseGenerativeNeuralEmulator):
+    def __init__(self, model):
+        super(CGenerativeModelNeuralEmulator, self).__init__()
+
+        self.model = None           # Neural Emulator Neural Network
+        self.model_path = model     # Path to the saved neural network file
+        self.output_dims = None
+        self.input_dims = None
+        self.initialize(model)
+        self.NN_result = t_tensor([])
+
+    @staticmethod
+    def get_name():
+        return "emu"
+
+    def initialize(self, model):
+        try:
+            self.model = torch.load(model, map_location='cpu')
+            self.output_dims = self.model.output_dim
+            self.input_dims = self.model.input_dim
+        except FileNotFoundError:
+            print("ERROR. FILE NOT FOUND. Failed to load neural emulator model from: " + self.model_path)
+            self.model = None
+
+    def generate(self, z, n):
+        self.NN_result = self.model(t_tensor(z))
+        return self.NN_result[:, 0:int(self.output_dims)]
+
+    def move_to_device(self, device):
+        raise NotImplementedError
+
+    def gradient(self):
+        if len(self.NN_result) > 0:
+            return self.NN_result.grad
+        else:
+            raise Exception("Invalid gradient. Make sure to call generate() before using gradient().")
+
+    def forward(self, x):
+        raise NotImplementedError
+
+    def num_flat_features(self, x):
+        raise NotImplementedError
+
+    def test(self, dataset):
+        raise NotImplementedError
+
+    def train(self, dataset, epochs, learning_rate):
+        raise NotImplementedError
+
+
---
+++ reaching_intent/main_viewer_bayesian_neural_emulator.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python3
+
+#################
+# SYSTEM IMPORTS
+#################
+from common.common import *
+import torch.nn
+import pybullet as p
+import time
+from samplers.CSamplerUniform import CSamplerUniform
+
+#######################################
+# GENERIC IMPORTS (no need to edit)
+#######################################
+from neural_emulators.CGenerativeModelNeuralEmulator import CGenerativeModelNeuralEmulator
+from reaching_intent.generative_models.CGenerativeModelSimulator import CGenerativeModelSimulator
+from reaching_intent.generative_models.CGenerativeModelSimulator import create_sim_params
+##############################
+
+
+#######################################
+# DEBUG DRAWING IMPORTS
+#######################################
+from utils.draw import draw_trajectory
+from utils.draw import draw_trajectory_diff
+from utils.draw import draw_point
+from utils.draw import draw_text
+
+
+###################################
+# APPLICATION SPECIFIC PARAMETERS (add/remove/edit parameters to fit your implementation)
+###################################
+sample_rate = 30    # Sensor sampling rate
+sim_time = 5        # Prediction time window in seconds
+n_dims = 3          # Point dimensionality
+n_points = sample_rate * sim_time
+
+# Generative model parameter limits: start volume (x,y,z), end volume (x,y,z), controller(Kp,Ki,Kd,Krep,iClamp)
+param_limits_min = t_tensor([-0.05, 0.30, -0.10, 0.25, -0.4, 0.20, 5, 0.005, 0, 0.10, 20])
+param_limits_max = t_tensor([-0.04, 0.31, -0.09, 0.90, 0.4, 0.21, 20, 0.010, 0, 0.11, 30])
+
+# Select the parameters that are considered nuisance and the parameters that are considered interesting
+latent_mask = t_tensor([0,0,0,1,1,1,0,0,0,0,0]) == 1    # We are interested in the end position
+nuisance_mask = t_tensor([0,0,0,1,1,1,0,0,0,0,0]) == 0  # The rest of the parameters are considered nuisance
+
+# Neural emulator path
+nn_model_path = "pytorch_models/ne_fc4_10k_MSE_in11_out450.pt"
+###################################
+
+
+###############
+# GENERIC CODE
+###############
+# Sampler to sample parameter values to generate trajectories
+param_sampler = CSamplerUniform({"min": param_limits_min, "max": param_limits_max})
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+# Load the neural emulator generative model
+neNEmulator = CGenerativeModelNeuralEmulator(nn_model_path)
+
+# Load simulator for visualization purposes only
+simulator_params = create_sim_params(sim_time=sim_time, sample_rate=sample_rate)
+neSimulator = CGenerativeModelSimulator(simulator_params)
+
+while p.isConnected(neSimulator.sim_id):
+    # Sample a point in the goal space.
+    params = param_sampler.sample(nsamples=1, params=None)
+    z = params[:, latent_mask]
+    n = params[:, nuisance_mask]
+
+    # Generate a trajectory with the simulator
+    traj_gt = neSimulator.generate(z, n)
+
+    # Generate a trajectory with the emulator
+    traj_gen = neNEmulator.generate(z.view(1, -1), n=None)[0]
+
+    # Show both trajectories and the difference
+    draw_trajectory(traj_gen.view(-1, 3), color=[1, 0, 0], width=2, physicsClientId=neSimulator.sim_id, draw_points=True)
+    draw_trajectory(traj_gt.view(-1, 3), color=[0, 1, 0], width=2, physicsClientId=neSimulator.sim_id, draw_points=True)
+    draw_trajectory_diff(traj_gen.view(-1, 3), traj_gt.view(-1, 3), color=[0, 0, 1], width=1, physicsClientId=neSimulator.sim_id)
+    draw_point(z[0], [0, 0, 1], size=0.05, width=5, physicsClientId=neSimulator.sim_id)
+    loss, loss_terms = neNEmulator.model.criterion(traj_gen.view(-1, 3), traj_gt.view(-1, 3))
+    draw_text("Test Gen: loss: %5.3f" % torch.sqrt(loss).mean().item(), traj_gen.view(-1, 3)[-1], neSimulator.sim_id, [1, 0, 0])
+    draw_text("Test GT", traj_gt.view(-1, 3)[-1], neSimulator.sim_id, [0, 1, 0])
+
+    # Wait for N or space keypress to generate next
+    while p.isConnected(neSimulator.sim_id):
+        keys = p.getKeyboardEvents(neSimulator.sim_id)
+        if ord('n') in keys and keys[ord('n')] & p.KEY_WAS_TRIGGERED:
+            break
+        time.sleep(0.01)
+
+    p.removeAllUserDebugItems()
+
