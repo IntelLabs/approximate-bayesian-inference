Fix MSE loss computation. Previously it was computing the sum of all point to point errors which led to non-interpretable loss.
Former-commit-id: affa6698dd4139425c22fd10037f0935541b9d49

neural_emulators/loss_functions.py
@@ -47,7 +47,7 @@ def loss_MSE(z, x):
 
     diff = (x - fz)
 
-    term3 = torch.sum((diff*diff), 1)
+    term3 = torch.mean((diff*diff), 1)
 
     loss = term3
 