da978bd916caec9d2471d70638dca20cb7f99191
Javier Felip Leon <javier.felip.leon@intel.com>
2022-06-10 16:11:51 -0700

    fix Tensor import for new torch versions
    Former-commit-id: 5ba6e538ac274baa28d60df2a8922fd103b17c3f

--- common/common.py
+++ common/common.py
@@ -1,7 +1,7 @@
 import math
 import numpy as np
 import torch
-from torch.tensor import Tensor as t_tensor
+from torch import Tensor as t_tensor
 # from torch import DoubleTensor as t_tensor
 from sys import float_info
 float_epsilon = float_info.max
